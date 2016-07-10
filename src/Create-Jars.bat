5381238e94a076de80942e08871210f62db59d6a
Isaiah Patton <isaiahpatton1@gmail.com>
2016-07-10 15:41:43 -0400

    Update Create-Jars.bat

--- src/Create-Jars.bat
+++ src/Create-Jars.bat
@@ -1,6 +1,5 @@
 @echo off
 set /p jarbuild=Build: 
-REM jar cvfm ZunoZap-0.0.1-SNAPSHOT-0%jarbuild%.jar MANIFEST.MF ZunoZap*.class
 jar cvfm ZunoZap-0.0.%jarbuild%.jar MANIFEST.MF *.class *.png
 jar cvfm ZunoZap-latest.jar MANIFEST.MF *.class *.png
 pause
